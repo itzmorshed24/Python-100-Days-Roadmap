3dd01e499f545d4d82ef5b1107487c20e2c1f070
itzmorshed24 <itzmorshed24@gmail.com>
2025-09-17 20:58:38 +0600

    Day-02-Typecasting-Input()-Print-Escape-Sequences-Comments

---
+++ Week-01-Fundamentals/Day-02-Typecasting-Input()-Print-Escape-Sequences-Comments/typecasting.py
@@ -0,0 +1,9 @@
+#integer to float
+number = 10
+print(float(number))
+
+#string to integer or float
+num_str = input("Enter Your Number: ")
+num_int = int(num_str)
+num_float = float(num_str)
+print(num_int,num_float)
