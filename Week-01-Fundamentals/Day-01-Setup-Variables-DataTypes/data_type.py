1fb702cb6f000b44741e5312aa90a242de58cbe6
itzmorshed24 <itzmorshed24@gmail.com>
2025-09-17 08:14:04 +0600

    Day-01: Python Setup, First Program, Variables, Data Types

---
+++ Week-01-Fundamentals/Day-01-Setup-Variables-DataTypes/data_type.py
@@ -0,0 +1,9 @@
+name = "Morshed"
+age = 25
+height = 5.3
+is_student = True
+
+print(type(name))
+print(type(age))
+print(type(height))
+print(type(is_student))
---
+++ Week-01-Fundamentals/Day-01-Setup-Variables-DataTypes/variables.py
@@ -0,0 +1,6 @@
+name = "Morshed"
+age = 25
+height = 5.4
+is_student = True
+
+print(name, age, height, is_student)
